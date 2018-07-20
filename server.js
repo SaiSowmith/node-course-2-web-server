442663ec42df29e8dfe5fc856ba21f6b983183ce
SaiSowmith <sowmithvajroju@gmail.com>
2018-07-20 10:09:33 +0530

    Add project page

--- server.js
+++ server.js
@@ -57,6 +57,14 @@ res.render('about.hbs',{
 });
 });
 
+app.get('/projects',(req,res)=>{
+res.render('projects.hbs',{
+    pageTitle : 'Projects'
+
+});
+});
+
+
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage:'Unable to handle request'
